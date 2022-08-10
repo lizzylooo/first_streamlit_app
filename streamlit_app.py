f84d163e58ee7e43855fbdec059fda2c6ef8046b
lizzylooo <110909833+lizzylooo@users.noreply.github.com>
2022-08-10 10:14:04 -0400

    Update streamlit_app.py
    incorporated pandas with the fruityvice data

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,9 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) #just writes data to the screen
+
+# inports the data from the response into pandas
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# displays the data in a table
+streamlit.dataframe(fruityvice_normalized)
