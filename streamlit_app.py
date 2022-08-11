50c84f9eba8d86e59691f48464e146a613ce2d26
lizzylooo <110909833+lizzylooo@users.noreply.github.com>
2022-08-11 11:27:21 -0400

    Update streamlit_app.py
    added function for SF insert

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,10 +57,18 @@ if streamlit.button('Get Fruit Load List'):
         my_data_rows = get_fruit_load_list()
         streamlit.dataframe(my_data_rows)
 
+#Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+         return "Thanks for adding " + new_fruit
+
+ass_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List':
+    my_cnx = snowflake.connector.connect(**streamlit.secrets{"snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streanlit.text(back_from_function)
 #New section to display fruityvice api response
-fruit_add = streamlit.text_input('What fruit would you like to add?')
+fruit_add = 
 streamlit.write('Thanks for adding ', fruit_add)
 
-#don't run anything past here while we troubleshoot
-streamlit.stop()
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
