ed15a494cdd2ce71a37f8bcc8e79bccafe74bcbb
lizzylooo <110909833+lizzylooo@users.noreply.github.com>
2022-08-11 11:17:38 -0400

    Update streamlit_app.py
    Added another function and button

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,20 +44,23 @@ try:
 except URLError as e:
     streamlit.error()
 
-#don't run anything past here while we troubleshoot
-streamlit.stop()
-
-
-#import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#snowflake related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
+
+#add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+        my_data_rows = get_fruit_load_list()
+        streamlit.dataframe(my_data_rows)
 
 #New section to display fruityvice api response
 fruit_add = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', fruit_add)
 
+#don't run anything past here while we troubleshoot
+streamlit.stop()
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
